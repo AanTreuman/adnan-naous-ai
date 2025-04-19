0a462ee18ed8d3a979dab831f7cd179909b08d32
Aan Trueman <aan.trueman0@outlook.com>
2025-04-19 22:06:33 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,3 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  env: {
-    OPENAI_API_KEY: process.env.OPENAI_API_KEY
-  }
-};
+const nextConfig = {};
 module.exports = nextConfig;
